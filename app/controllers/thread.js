5c587e5232e61728ab456cbcd5ae3dbf8f4c2e34
Catalizcs <rfechinonguyen@gmail.com>
2020-03-22 11:17:36 +0700

    Update thread.js

--- app/controllers/thread.js
+++ app/controllers/thread.js
@@ -1,4 +1,4 @@
-const logger = require("@logger");
+const logger = require("~logger");
 module.exports = function ({ models, api }) {
     const Thread = models.use('thread');
     function getThreads(where = {}) {
