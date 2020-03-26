c0af415f8aa4b9c68c09db2424d1d1d7cb417142
Catalizcs <rfechinonguyen@gmail.com>
2020-03-26 13:51:25 +0700

    Update model.js

--- database/model.js
+++ database/model.js
@@ -1,8 +1,8 @@
 module.exports = function ({ Sequelize, sequelize }) {
     const force = process.env.NODE_ENV == 'development';
-    const user = require("./database/models/user")({ sequelize, Sequelize }),
-        thread = require("./database/models/thread")({ sequelize, Sequelize }),
-        log = require("./database/models/log")({ sequelize, Sequelize });
+    const user = require("./models/user")({ sequelize, Sequelize }),
+        thread = require("./models/thread")({ sequelize, Sequelize }),
+        log = require("./models/log")({ sequelize, Sequelize });
     user.sync({force});
     thread.sync({force});
     log.sync({force});
